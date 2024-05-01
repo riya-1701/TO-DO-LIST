a48e98d979b8e59054b326f9b82b44767a27c898
Riya Srivastava <riyasrivastava1701@gmail.com>
2024-05-02 00:33:58 +0530

    last update

--- code/api/routes/user_route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const route = express();
-
-const userController = require('../controllers/user_controller');
-
-route.post('/login',userController.login);
-
-module.exports = route;
